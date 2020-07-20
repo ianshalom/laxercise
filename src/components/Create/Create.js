8e78b9bf13f5033ad03a9cf314b91e21374e26b4
Ian Foo <ianfoo@Ians-MacBook-Pro.local>
2020-07-20 13:51:13 +0800

    Added react date picker

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import axios from "../../axios-create";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./Create.css";
 
 const CreateSession = (props) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, control } = useForm();
+  const [startDate, setStartDate] = useState();
+
+  // const onChange = (date) => {
+  //   setStartDate(date);
+  //   console.log(date);
+  // };
+
+  let handleColor = (time) => {
+    return time.getHours() > 12 ? "text-success" : "text-error";
+  };
 
   const onSubmit = (data) => {
     console.log(data);
+    setStartDate(data.dateSelected.toDateString());
+
     axios
       .post("/activity.json", data)
       .then((res) => {
@@ -18,9 +33,9 @@ const CreateSession = (props) => {
   };
 
   return (
-    <div>
-      <h1>Host an activity</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className={"Form"}>
+      <h1>Date: </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className={"input"}>
         <input type="text" name="title" placeholder="title" ref={register} />
         <textarea
           type="text"
@@ -28,8 +43,47 @@ const CreateSession = (props) => {
           placeholder="description"
           ref={register}
         />
-        <input type="text" name="date" placeholder="date" ref={register} />
-
+        {/* <Controller
+          as={
+            <DatePicker
+              dateFormat="d MMM yyyy"
+              showTimeSelect
+              // selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              timeClassName={handleColor}
+            />
+          }
+          placeholderText="Select date and time"
+          control={control}
+          // valueName="selected"
+          name="dateAndTime"
+          ref={register}
+        /> */}
+        <Controller
+          as={
+            <DatePicker
+              dateFormat="d MMM yyyy"
+              minDate={new Date()}
+              showTimeSelect={false}
+              todayButton="Today"
+              dropdownMode="select"
+              isClearable
+              placeholderText="Click to select date"
+              shouldCloseOnSelect
+            />
+          }
+          control={control}
+          register={register({ required: true })}
+          name="dateSelected"
+          valueName="selected" // DateSelect value's name is selected
+          onChange={([selected]) => {
+            return selected;
+          }}
+          rules={{
+            required: true,
+          }}
+        />
+        <h2>Date and Time {startDate ? startDate : null} </h2>
         <input
           type="text"
           name="location"
